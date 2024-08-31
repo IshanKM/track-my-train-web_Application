c537c334665875da4a22c49a686e9fc122ed15d3
Ishan Madushanka <ishanmadushanka.sg@gmail.com>
2024-08-31 12:32:35 +0530

    update docker file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: 80, // The port inside the container
+    host: "0.0.0.0", // Allows external access
+  },
+});
