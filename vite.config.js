5e3f649f450b2a14624450ed05cb36cf1a6388d9
Ishan Madushanka <ishanmadushanka.sg@gmail.com>
2024-08-31 13:55:07 +0530

    change vite.congif.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
-export default {
-  server: {
-    port: 5174, // or any other port not reserved
-  },
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+});
